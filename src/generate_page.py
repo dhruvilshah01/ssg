7840e506670e0efdd6b177476ca0a8e5aab0cb65
dhruvilshah01 <dhruvil528@gmail.com>
2025-05-15 16:54:45 -0400

    making changes for static site hosting with github

--- src/generate_page.py
+++ src/generate_page.py
@@ -5,7 +5,7 @@ from convert_markdown import markdown_to_blocks, block_to_block_type, markdown_t
 from blocks import BlockType
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     dir_list = os.listdir(dir_path_content)
     for directory in dir_list:
         curr_content_path = os.path.join(dir_path_content,directory)
@@ -18,17 +18,17 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             #print new destination directory path
 
             #call generate page
-            generate_page(curr_content_path,template_path,curr_dir_path)
+            generate_page(curr_content_path,template_path,curr_dir_path, basepath)
         #it is a directory
         else:
             curr_dir_path = os.path.join(dest_dir_path, directory)
             os.mkdir(curr_dir_path)
-            generate_pages_recursive(curr_content_path, template_path, curr_dir_path)
+            generate_pages_recursive(curr_content_path, template_path, curr_dir_path, basepath)
     return
 
     
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     #print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     from_path_file = open(from_path, "r")
@@ -44,6 +44,8 @@ def generate_page(from_path, template_path, dest_path):
 
     replaced_title = template_content.replace("{{ Title }}", title)
     content = replaced_title.replace("{{ Content }}", content)
+    content = template_content.replace('href="/',f'href="{basepath}')
+    content = template_content.replace('src="/', f'src="{basepath}')
 
     
     
@@ -68,11 +70,12 @@ def extract_title(markdown):
         
 
 
-def copy_static_files():
+def copy_static_files(copy_to_dir):
     #purge and recreate public directory
-    shutil.rmtree("./public")
-    os.makedirs("./public")
-    copy_static_files_helper("./static","./public")
+    if(os.path.exists(copy_to_dir)):
+        shutil.rmtree(copy_to_dir)
+    os.makedirs(copy_to_dir)
+    copy_static_files_helper("./static",copy_to_dir)
 
 def copy_static_files_helper(src_path, dest_path):
     dir_list = os.listdir(src_path)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
+import sys
 from generate_page import copy_static_files, generate_pages_recursive
+
 
 def main():
     # copy static files from static to public
-    copy_static_files()
+    copy_static_files("./docs")
+
     # generate a page
-    generate_pages_recursive("./content", "template.html", "./public")
+    basepath = "/"
+    if(sys.argv[1]):
+        basepath = sys.argv[1]
+    
+    generate_pages_recursive("./content", "template.html", "./docs", basepath)
 
 
         
